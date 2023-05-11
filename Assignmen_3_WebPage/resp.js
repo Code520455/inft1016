43563aa00c3a1d9b44843c79a420a669726b5f54
Code520455 <130625646+Code520455@users.noreply.github.com>
2023-05-11 12:48:12 +0930

    main

---
+++ Assignmen_3_WebPage/resp.js
@@ -0,0 +1,22 @@
+burger = document.getElementsByClassName('burger');
+navbar = document.querySelector('navbar');
+navlist = document.querySelector('navlist');
+rightnav = document.querySelector('rightnav');
+
+
+burger.addEventListener("click", function(){
+
+    // if (document.getElementsByClassName("secondhalf").style.dispaly === "none") {
+    //     document.getElementsByClassName("secondhalf").style.dispaly =="block";
+    //   } else {
+    //     document.getElementsByClassName("secondhalf").style.dispaly = "none";
+    //   }
+    
+
+
+    // rightnav.classList.toggle('v-class-resp');
+
+    // navlist.classList.toggle('v-class-resp');
+
+    // navbar.classList.toggle('h-nav-resp')
+});
